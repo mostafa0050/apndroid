4ce904367c84200ececff7ac92416706ac759960
zeldigas <zeldigas@gmail.com>
2011-04-08 00:53:07 +0400

    generated files

--- gen/com/twofortyfouram/locale/platform/R.java
+++ gen/com/twofortyfouram/locale/platform/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.google.code.apndroid;
+package com.twofortyfouram.locale.platform;
 
 public final class R {
     public static final class array {
