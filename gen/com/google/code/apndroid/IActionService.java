3419c926b3ac1c67180382ae591a6e4e34136d31
Martin Adamek <martin.adamek@gmail.com>
2011-07-16 20:47:33 +0200

    IDEA project metadata

--- gen/com/google/code/apndroid/IActionService.java
+++ gen/com/google/code/apndroid/IActionService.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /home/pavlov/projects/apndroid/trunk/src/com/google/code/apndroid/IActionService.aidl
+ * Original file: /Users/martin/sources/apndroid_lite/src/com/google/code/apndroid/IActionService.aidl
  */
 package com.google.code.apndroid;
 public interface IActionService extends android.os.IInterface
