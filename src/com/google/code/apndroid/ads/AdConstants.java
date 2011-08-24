38958881475ab122bcaccb26955dd18b304a2169
zeldigas <zeldigas@gmail.com>
2011-08-24 07:32:27 +0400

    Removed MoPub constant

--- src/com/google/code/apndroid/ads/AdConstants.java
+++ src/com/google/code/apndroid/ads/AdConstants.java
@@ -2,7 +2,6 @@ package com.google.code.apndroid.ads;
 
 class AdConstants {
 
-    static final String MOPUB_AD_UNIT_ID = "<REPLACE_ME>";
     static final String ADMOB_AD_UNIT_ID = "<REPLACE_ME>";
 
 }
